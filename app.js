493e08d58fdeea3f69e5f544d9e737befd5d7561
ooeid <ooeid.omar@gmail.com>
2023-05-15 09:46:05 +0300

    init

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
 export const options = {
   vus: 5,
   duration: '10s',
-  // ext: {
-  //   loadimpact: {
-  //     distribution: {
-  //       distributionLabel1: { loadZone: 'amazon:bh:bahrain', percent: 50 },
-  //       distributionLabel2: { loadZone: 'amazon:fr:paris', percent: 50 },
-  //     },
-  //   },
-  // },
+  ext: {
+    loadimpact: {
+      distribution: {
+        distributionLabel1: { loadZone: 'amazon:bh:bahrain', percent: 50 },
+        distributionLabel2: { loadZone: 'amazon:fr:paris', percent: 50 },
+      },
+    },
+  },
 };
 
 export function handleSummary(data) {
@@ -25,7 +25,7 @@ export function handleSummary(data) {
 }
 
 export default function () {
-  http.get('');
+  http.get('https://ideas.media.gov.sa/');
 
 }
 
